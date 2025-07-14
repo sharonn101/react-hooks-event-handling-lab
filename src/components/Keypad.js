7d2f552c3ab512a4072cfa6ac4de415cf04268c5
Sharon <sharonirungu2006@gmail.com>
2025-07-15 00:55:53 +0300

    adds events

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleKeyPress() {
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleKeyPress}/>
+        </div>
     )
 }
 
